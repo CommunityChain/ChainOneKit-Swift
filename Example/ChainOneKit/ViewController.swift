3880d31908359a4dad3f706ac4ede71bfab591ab
TangXiaoDe <xiaodehappy@foxmail.com>
2019-05-20 15:33:32 +0800

    feat: Pod Pod更新

--- Example/ChainOneKit/ViewController.swift
+++ Example/ChainOneKit/ViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import ChainOneKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.random
+        
     }
+    
+    
 
 
 }
---
+++ Example/Pods/Target Support Files/ChainOneKit-Swift/ChainOneKit-Swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ChainOneKit_Swift : NSObject
+@end
+@implementation PodsDummy_ChainOneKit_Swift
+@end
