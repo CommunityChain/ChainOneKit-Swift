e0534c5079b917558866a4f7c98685a246c0496c
TangXiaoDe <xiaodehappy@foxmail.com>
2019-05-23 09:47:03 +0800

    feat: Extension 添加本地化的字符串加载扩展

---
+++ ChainOneKit/Source/Extension/Foundation+Extension/String+Localizable.swift
@@ -0,0 +1,23 @@
+//
+//  String+Localizable.swift
+//  ProjectTemplate
+//
+//  Created by 小唐 on 2019/5/22.
+//  Copyright © 2019 ProjectTemplate. All rights reserved.
+//
+//  字符串本地化
+
+import Foundation
+
+extension String {
+    
+    /// 使用Localizable.strings命名时
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    /// 使用自定义的命名时，如Internation.strings时传入Internation
+    func localized(tableName: String) -> String {
+        return NSLocalizedString(self, tableName: tableName, bundle: Bundle.main, comment: "")
+    }
+    
+}
