45cfa0b5c4428c76cd5db1d504a54c31bce2d38f
TangXiaoDe <xiaodehappy@foxmail.com>
2020-12-01 15:45:11 +0800

    Extension: 扩展库部分更新

--- ChainOneKit/Source/Extension/Foundation+Extension/NSMutableAttributedString+Extension.swift
+++ ChainOneKit/Source/Extension/Foundation+Extension/NSMutableAttributedString+Extension.swift
@@ -33,3 +33,21 @@ public extension NSMutableAttributedString {
     }
 
 }
+
+public extension NSMutableAttributedString {
+    
+    /// 快捷属性
+    class func mutableAttribute(str: String, font: UIFont, color: UIColor) -> NSMutableAttributedString {
+        let attText: NSMutableAttributedString = NSMutableAttributedString.init(string: str, attributes: [NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor: color])
+        return attText
+    }
+    class func mutableAttribute(_ atts: [(str: String, font: UIFont, color: UIColor)]) -> NSMutableAttributedString {
+        let attText: NSMutableAttributedString = NSMutableAttributedString.init()
+        for att in atts {
+            let itemAtt = NSAttributedString.init(string: att.str, attributes: [NSAttributedString.Key.font: att.font, NSAttributedString.Key.foregroundColor: att.color])
+            attText.append(itemAtt)
+        }
+        return attText
+    }
+
+}
