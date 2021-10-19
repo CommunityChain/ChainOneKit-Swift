0a623fa18f78bfac8021ff29252ffbf84b7673ba
TangXiaoDe <xiaodehappy@foxmail.com>
2021-10-19 10:05:51 +0800

    Util: Double扩展库更新

--- ChainOneKit/Source/Extension/Foundation+Extension/Double+Extension.swift
+++ ChainOneKit/Source/Extension/Foundation+Extension/Double+Extension.swift
@@ -12,7 +12,7 @@ import Foundation
 public extension Double {
 
     /// 小数部分的有效位数处理
-    func decimalValidDigitsProcess(digits: Int = 4) -> String {
+    func decimalValidDigitsProcess(digits: Int = 4, mode: NumberFormatter.RoundingMode = .down) -> String {
 
         // 0.00001 使用"\(value)" 或 String(stringInterpolationSegment: value) 会使用科学计数法标记
         // 514.25479966 使用 String(format: "%f", value) 方式会进行舍入
@@ -22,11 +22,14 @@ public extension Double {
         let formatter = NumberFormatter()
         formatter.numberStyle = NumberFormatter.Style.decimal
         formatter.usesGroupingSeparator = false     // 是否显示分隔符
-        formatter.roundingMode = .down              // 舍入方式
+        formatter.roundingMode = mode              // 舍入方式
         //formatter.roundingIncrement = 0.0001        // 保留到哪位小数
         formatter.maximumFractionDigits = digits         // 小数最少位数
 
-        return formatter.string(from: NSNumber(value: self))!
+        // let result = 18.62321805 - 4.29119543 // result    Double    14.332022619999998
+        // 使用NSNumber是14.332022619999998，而使用NSDecimalNumber则是14.33202262，其处理结果根据roundingMode会有差异
+        //return formatter.string(from: NSNumber(value: self))!
+        return formatter.string(from: NSDecimalNumber(value: self))!
     }
 
 }
